c3628aca631dcd85398d516d86d2d21a346b4059
breeze6989 <breeze6989@naver.com>
2025-07-29 18:06:16 +0900

    Delete web directory

--- web/app/models/Layout.py
+++
@@ -1,8 +0,0 @@
-from ..extensions import db
-
-class Layout(db.Model):
-    """그리드 배열 (3×3 / 4×3) 설정"""
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), unique=True)
-    rows = db.Column(db.Integer)
-    cols = db.Column
